055cafcef903d058795c24362aee4f68c72000dd
ewainy <77060368+ewainy@users.noreply.github.com>
2021-09-13 16:51:51 +0100

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,21 @@
+
+/*Code a project that:
+
+Outputs “HelloWorld” to the console.
+Return “HelloWorld” as a String.
+Has a method that takes a String parameter and outputs this to the console.*/
+
+
+
+public class HelloWorld {
+	
+	public static String printHelloWorld() {
+	     String message = "Hello World!";
+	     System.out.println(message);
+		return message;
+	 }
+
+	public static void main(String[] args) {
+		printHelloWorld();
+	}
+}
