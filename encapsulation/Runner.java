886e956bd6aafef00c230fa23b233c22c1ea1bf8
ewainy <77060368+ewainy@users.noreply.github.com>
2021-09-16 16:14:41 +0100

    Create Runner.java

---
+++ encapsulation/Runner.java
@@ -0,0 +1,19 @@
+package encapsulation;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		
+		Encapsulation example1 = new Encapsulation();
+		example1.setName("Amy");
+		
+	
+		
+		System.out.println("Name: " + example1.getName());
+		
+		
+		System.out.println(example1.toString());
+	}
+
+	
+}
