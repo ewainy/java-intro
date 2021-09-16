7fb257d35c61e1f6c88358cb14184c42299eba95
ewainy <77060368+ewainy@users.noreply.github.com>
2021-09-16 12:10:34 +0100

    Create Runner.java

---
+++ Runner.java
@@ -0,0 +1,29 @@
+
+public class Runner {
+
+	public static void main(String[] args) {
+		Person Emma = new Person ("Emma", 154);
+		Person Chris = new Person ("Chris", 180);
+		Person Emily = new Person ("Emily", 172);
+		Person Rhys = new Person ("Rhys", 500);
+		Person Hero = new Person ("Hero", 20);
+		
+
+		Emma.greet();
+		Emma.height();
+		
+		Chris.greet();
+		Chris.height();
+		
+		Emily.greet();
+		Emily.height();
+		
+		Rhys.greet();
+		Rhys.height();
+		
+		Hero.greet();
+		Hero.height();
+		
+	}
+
+}
